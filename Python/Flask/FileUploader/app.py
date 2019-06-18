e4d99e774add6c9cad3af82d3295d1d2f1e018b6
Juan Pablo Lozano <lozanotux@gmail.com>
2019-06-18 12:33:53 -0300

    Create app.py
    App Flask for File Uploader

---
+++ Python/Flask/FileUploader/app.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+# Path to upload the files
+UPLOAD_FOLDER = '/tmp'
+# Types of files allowed to upload
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'zip'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Function to check the file types allowed (return True or False)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', filename=filename))
+        else:
+            flash('Error al subir el archivo, solo las extensiones bla bla bla es pertmitido')
+            return redirect(url_for('upload_file', filename=filename))
+    return render_template('upload.html')
+
+app.secret_key = 'ABC123SPAKSPASJHSAIH'
+
+if __name__ == '__main__':
+   app.run(debug = True)
