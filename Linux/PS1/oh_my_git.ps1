54e2dce0d2bfc72480a1f0f9bf5cf819544bc042
Juan Pablo Lozano <lozanotux@gmail.com>
2020-03-12 15:11:41 -0300

    Create oh_my_git.ps1
    An awesome look amazing bash prompt in PS1

---
+++ Linux/PS1/oh_my_git.ps1
@@ -0,0 +1,48 @@
+# get current branch in git repo
+function parse_git_branch() {
+        BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+        if [ ! "${BRANCH}" == "" ]
+        then
+                STAT=`parse_git_dirty`
+                echo " ⛕  ${BRANCH}${STAT} "
+        else
+                echo ""
+        fi
+}
+
+# get current status of git repo
+function parse_git_dirty {
+        status=`git status 2>&1 | tee`
+        dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+        untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+        ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
+        newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+        renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+        deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+        bits=''
+        if [ "${renamed}" == "0" ]; then
+                bits=">${bits}"
+        fi
+        if [ "${ahead}" == "0" ]; then
+                bits="*${bits}"
+        fi
+        if [ "${newfile}" == "0" ]; then
+                bits="+${bits}"
+        fi
+        if [ "${untracked}" == "0" ]; then
+                bits="?${bits}"
+        fi
+        if [ "${deleted}" == "0" ]; then
+                bits="x${bits}"
+        fi
+        if [ "${dirty}" == "0" ]; then
+                bits="!${bits}"
+        fi
+        if [ ! "${bits}" == "" ]; then
+                echo " ${bits}"
+        else
+                echo ""
+        fi
+}
+
+export PS1="\[\e[30;43m\] 🔌  \u \[\e[m\]\[\e[33;44m\]▶\[\e[m\]\[\e[37;44m\] 💻  \[\e[m\]\[\e[44m\]\h \[\e[m\]\[\e[34;46m\]▶\[\e[m\]\[\e[46m\] \w \[\e[m\]\[\e[42m\]\`parse_git_branch\`\[\e[m\]\[\e[36m\]▶ \[\e[m\] "
